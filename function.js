dceffa95c6411070a14f9c8b7cea7305a251c2aa
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-02-18 02:42:30 +0600

    parallelogram area calculate done

--- function.js
+++ function.js
@@ -5,9 +5,10 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
     const triangleBaseValue = parseInt(triangleBaseString);
     const triangleHeight = document.getElementById('triangle-height');
     const triangleHeightString = triangleHeight.value ;
+    const triangleHeightValue = parseInt(triangleHeightString);
+    
     triangleBase.value ='';
     triangleHeight.value = '';
-    const triangleHeightValue = parseInt(triangleHeightString);
     if(isNaN(triangleBaseValue) || isNaN(triangleHeightValue)){
         alert('please inter number')
     }
@@ -17,7 +18,55 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
     }
     const triangleArea = 0.5 * triangleBaseValue * triangleHeightValue;
       
-    const outputArea = document.getElementById('output');
+    const outputArea = document.getElementById('triangle-output');
     const show = outputArea.innerText =triangleArea;
+    
+})
+//For Rectangle
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    const rectangleWidth = document.getElementById('rectangle-width');
+    const rectangleWidthString = rectangleWidth.value ;
+    const rectangleWidthValue = parseInt(rectangleWidthString);
+    const rectangleLength = document.getElementById('rectangle-length');
+    const rectangleLengthString = rectangleLength.value ;
+    rectangleWidth.value ='';
+    rectangleLength.value = '';
+    const rectangleLengthValue = parseInt(rectangleLengthString);
+    if(isNaN(rectangleWidthValue) || isNaN(rectangleLengthValue)){
+        alert('please inter number')
+    }
+    else if(rectangleWidthValue<0 || rectangleLengthValue<0)
+    {
+        alert('please inter positive number')
+    }
+    const rectangleArea =rectangleWidthValue * rectangleLengthValue;
+      
+    const outputArea = document.getElementById('rectangle-output');
+    const show = outputArea.innerText =rectangleArea;
+    
+})
+
+// for parallelogram
+document.getElementById('btn-parallelogram').addEventListener('click', function(){
+    const parallelogramBase = document.getElementById('parallelogram-base');
+    const parallelogramBaseString = parallelogramBase.value ;
+    const parallelogramBaseValue = parseInt(parallelogramBaseString);
+    const parallelogramHeight = document.getElementById('parallelogram-height');
+    const parallelogramHeightString = parallelogramHeight.value ;
+    const parallelogramHeightValue = parseInt(parallelogramHeightString);
+    
+    parallelogramBase.value ='';
+    parallelogramHeight.value = '';
+    if(isNaN(parallelogramBaseValue) || isNaN(parallelogramHeightValue)){
+        alert('please inter number')
+    }
+    else if(parallelogramBaseValue<0 || parallelogramHeightValue<0)
+    {
+        alert('please inter positive number')
+    }
+    const parallelogramArea =  parallelogramBaseValue * parallelogramHeightValue;
+      
+    const outputArea = document.getElementById('parallelogram-output');
+    const show = outputArea.innerText =parallelogramArea;
     
 })
